d8b19c1878e68a4781aa59a41e339a57d5872782
sumo431 <sokada1@csuchico.edu>
2026-01-13 17:38:56 -0800

    Initial Commit: Roulette21 Spring Boot project

---
+++ src/main/java/com/example/roulette21/Roulette21Application.java
@@ -0,0 +1,13 @@
+package com.example.roulette21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roulette21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Roulette21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roulette21/Roulette21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roulette21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roulette21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
